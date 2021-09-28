d26a66a0329050395a4e25fc42b5c243ac0eef10
Huriel <huri3l.lopes@gmail.com>
2021-09-27 23:15:13 -0300

    feat: ending app

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,79 @@
+import { useEffect, useState } from 'react';
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+import totalImg from '../../assets/total.svg';
+import { useTransactions } from '../../hooks/useTransactions';
+import { Container, TotalContainer } from './styles';
+
+export function Summary() {
+  const { transactions } = useTransactions();
+  const [isTotalPositive, setIsTotalPositive] = useState(false);
+
+  const summary = transactions.reduce(
+    (acc, transaction) => {
+      if (transaction.type === 'deposit') {
+        acc.deposits += transaction.amount;
+        acc.total += transaction.amount;
+      } else {
+        acc.withdraws += transaction.amount;
+        acc.total -= transaction.amount;
+      }
+
+      return acc;
+    },
+    {
+      deposits: 0,
+      withdraws: 0,
+      total: 0,
+    },
+  );
+
+  useEffect(() => {
+    setIsTotalPositive(summary.total >= 0 ? true : false);
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [transactions]);
+
+  return (
+    <Container>
+      <div>
+        <header>
+          <p>Entradas</p>
+          <img src={incomeImg} alt="Entradas" />
+        </header>
+        <strong>
+          {new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL',
+          }).format(summary.deposits)}
+        </strong>
+      </div>
+      <div>
+        <header>
+          <p>Saídas</p>
+          <img src={outcomeImg} alt="Saídas" />
+        </header>
+        <strong>
+          -{' '}
+          {new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL',
+          }).format(summary.withdraws)}
+        </strong>
+      </div>
+      <TotalContainer className="highlight-background" isTotalPositive={isTotalPositive}>
+        <header>
+          <p>Total</p>
+          <img src={totalImg} alt="Entradas" />
+        </header>
+        <strong>
+          {' '}
+          {new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL',
+          }).format(summary.total)}
+        </strong>
+      </TotalContainer>
+    </Container>
+  );
+}
